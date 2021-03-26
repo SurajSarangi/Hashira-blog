1b4c03ea37570193e0ceb69960808c6ea0225d3f
SurajSarangi <surajsarangi1@gmail.com>
2021-03-26 23:33:42 +0530

    added dynamic variables in templates in app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	const title = "Welcome to dojo blog";
+	const num = 50;
   return (
     <div className="App">
       
       <div className="content">
-          <h1>App Component</h1>
-          
+          <h1>{ title }</h1>
+          <p>Liked { num } times</p>
       </div>
     </div>
   );
