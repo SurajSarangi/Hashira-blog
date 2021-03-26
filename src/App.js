612663812a13ecd939a4be34020a6cc1ad3c8315
SurajSarangi <surajsarangi1@gmail.com>
2021-03-26 23:56:20 +0530

    added components and embedded them in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-	const title = "Welcome to dojo blog";
-	const num = 50;
   return (
     <div className="App">
-      
+      <Navbar />
       <div className="content">
-          <h1>{ title }</h1>
-          <p>Liked { num } times</p>
+      	  <Home />
       </div>
     </div>
   );
