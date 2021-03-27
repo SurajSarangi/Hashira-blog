4b7a26a3b5a81d1f44c00c798d5499ff1540f565
SurajSarangi <surajsarangi1@gmail.com>
2021-03-27 19:05:41 +0530

    Changed blog to Hashira and types

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 
-const Home = () =>{
+const Home = () => {
 
-	const [name, setName] = useState('rengoku');
-	const [type, setType] = useState('flame');
-
-	const handleClick = () =>{
-		setName('shinazugawa');
-		setType('wind');
-	}
+	// eslint-disable-next-line no-unused-vars
+	const [blogs, setBlogs] = useState([
+		{ title: 'Wind Hashira', body: 'lorem ipsum...', author: 'Shinazugawa', id: 1 },
+    	{ title: 'Flame Hashira', body: 'lorem ipsum...', author: 'Rengoku', id: 2 },
+    	{ title: 'Water Hashira', body: 'lorem ipsum...', author: 'Tomioka', id: 3 }
+	]);
 
 	return(
 		<div className="home">
-			<h2>Homepage</h2>
-			<p>{name} is {type} hashira</p>
-			<button onClick = {handleClick}>Click Me</button>
+			{blogs.map(blog => (
+				<div className="blog-preview" key={blog.id}>
+					<h2>{blog.title}</h2>
+					<p>Written by <span>{blog.author}</span></p>
+				</div>
+			))}
 		</div>
 	);
 }
