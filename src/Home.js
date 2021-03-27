ed830b17b9fe52687dbe172a2b6d1b34d113a6c9
SurajSarangi <surajsarangi1@gmail.com>
2021-03-27 12:27:23 +0530

    onclick with event and parameters. removed an import from app

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () =>{
+
+	const handleClick = (e) =>{
+		console.log("Hello", e);
+	}
+
+	const handleClickAgain = (name, e) => {
+		console.log('hello '+name, e.target);
+	}
+
 	return(
 		<div className="home">
 			<h2>Homepage</h2>
+				<button onClick = {handleClick}>Click Me</button>
+				<button onClick = {(e)=>handleClickAgain('rengoku', e)}>Click Me Again</button>
 		</div>
 	);
 }
