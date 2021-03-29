8f998c74a502f273f1740b813d43fdbf39e4e4c2
SurajSarangi <surajsarangi1@gmail.com>
2021-03-29 21:24:13 +0530

    added react routers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-      	  <Home />
-      </div>
-    </div>
-  );
+return (
+	<Router>	
+		<div className="App">
+			<Navbar />
+			<div className="content">
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/create">
+						<Create />
+					</Route>
+				</Switch>
+			</div>
+		</div>
+	</Router>
+    );
 }
 
 export default App;
