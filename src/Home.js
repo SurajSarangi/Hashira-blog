4a4d519318e5133fa4c94b342077115b9a723ec6
SurajSarangi <surajsarangi1@gmail.com>
2021-03-29 20:05:23 +0530

    added a conditional error message

--- src/Home.js
+++ src/Home.js
@@ -5,22 +5,32 @@ const Home = () => {
 	// eslint-disable-next-line no-unused-vars
 	const [blogs, setBlogs] = useState(null);
 	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
 	useEffect(()=>{
 		setTimeout(()=>{
 			fetch('http://localhost:5000/blogs')
 			.then(res => {
+				if (!res.ok) {
+					throw Error('Could not fetch data from source');
+				}
 				return res.json();
 			})
 			.then(data => {
 				setBlogs(data);
 				setIsPending(false);
+				setError(null);
 			})
+			.catch(err =>{
+				setIsPending(false);
+				setError(err.message);
+			});
 		}, 1000);
 	},[]);
 
 	return(
 		<div className="home">
+			{error && <div>error</div>}
 			{isPending && <div>Loading...</div> }
 			{blogs && <BlogList blogs={blogs} title="All Blogs"/>}
 		</div>
