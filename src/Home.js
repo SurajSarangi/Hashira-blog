907ef94b0da23c5b2db2dfd80630caf44c8242a8
SurajSarangi <surajsarangi1@gmail.com>
2021-03-29 19:46:17 +0530

    added a conditional loading msg with timeout

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,24 @@ const Home = () => {
 
 	// eslint-disable-next-line no-unused-vars
 	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIsPending] = useState(true);
 
 	useEffect(()=>{
-		fetch('http://localhost:5000/blogs')
+		setTimeout(()=>{
+			fetch('http://localhost:5000/blogs')
 			.then(res => {
 				return res.json();
 			})
 			.then(data => {
 				setBlogs(data);
+				setIsPending(false);
 			})
+		}, 1000);
 	},[]);
 
 	return(
 		<div className="home">
+			{isPending && <div>Loading...</div> }
 			{blogs && <BlogList blogs={blogs} title="All Blogs"/>}
 		</div>
 	);
