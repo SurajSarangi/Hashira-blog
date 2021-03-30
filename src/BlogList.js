eb2302781d6c66d2c9bc12695ec7466175edd440
SurajSarangi <surajsarangi1@gmail.com>
2021-03-30 14:25:28 +0530

    display each blog when clicked

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title}) => {
     return (
         <div className="blog-list">
             <h2 style={{color : "darkcyan", paddingLeft: "16px"}}>{title}</h2>
             {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by <span>{blog.author}</span></p>
+                    <Link to = {`blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by <span>{blog.author}</span></p>
+                    </Link>
                 </div>
             ))}
         </div>
