226b693fc53b2049f55b3b2b2a282f966a3e913c
SurajSarangi <surajsarangi1@gmail.com>
2021-03-30 23:56:56 +0530

    updated create.js to store value in variables

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Obanai');
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input 
+                    type = 'text'
+                    required
+                    value = { title }
+                    onChange = {(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body:</label>
+                <textarea 
+                    type = 'text'
+                    required
+                    value = { body }
+                    onChange = {(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Author:</label>
+                <select
+                    value = { author }
+                    onChange = {(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Obanai">Obanai</option>
+                    <option value="Mitsuri">Mitsuri</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
      );
 }
