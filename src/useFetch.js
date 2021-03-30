bb6df5aeed3e9b43656fc4d41e51266c1db0f20c
SurajSarangi <surajsarangi1@gmail.com>
2021-03-30 13:48:23 +0530

    useEffect cleanup with AbortController

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,29 +5,38 @@ const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
-
-    const getfetch = async(url) =>{
-        const res = await fetch(url);
+    
+    const getfetch = async(url, abortCont) =>{
+        const res = await fetch(url, { signal: abortCont.signal });
         if(!res.ok){
             throw Error('Could not fetch data from source');
         }
         const val = await res.json()
         return val;
     };
-
+    
     useEffect(() => {
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            getfetch(url)
+            getfetch(url, abortCont)
                 .then(data => {
                     setIsPending(false);
                     setData(data);
                     setError(null);
                 })
                 .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
+                    if (err.name === 'AbortError'){
+                        console.log('fetch aborted');
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                    }
                 })
         }, 200);
+
+        return ()=> abortCont.abort();
+
     }, [url]);
 
     return { data, isPending, error };
